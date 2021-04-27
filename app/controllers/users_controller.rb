044de9754dbd442cbe8f50089ad341379dd8f269
anonymous <git.rintaro@gmail.com>
2021-04-27 07:26:29 +0000

    fix: permission

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,7 +57,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:name, :email, :password, :password_confirmation, :discordid, :discordname)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :discordid, :discordname)
   end
   
 end
