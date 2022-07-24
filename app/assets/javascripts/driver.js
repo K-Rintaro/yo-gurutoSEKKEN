1c1e6e3ca74137d197b3c2fcbaf910ef3afd7e60
K-Rintaro <k.rintaro1@icloud.com>
2022-07-24 13:49:38 +0900

    aaa

--- app/assets/javascripts/driver.js
+++ app/assets/javascripts/driver.js
@@ -43,6 +43,25 @@ L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
   attribution: '<a href="https://maps.gsi.go.jp/development/ichiran.html" target="_blank">国土地理院</a>',
 }).addTo(mymap);
 
+// 音声読み上げON/OFFフラグ
+let speakCommentFlg = 0;
+
+// 音声読み上げスイッチ
+function speakSwitch() {
+  let speakMsg = new SpeechSynthesisUtterance('コメント読み上げをオンにしました。');
+  speakMsg.lang = 'ja-JP';
+  speakMsg.rate = 1.0;
+  if ( speakCommentFlg == 1 ) {
+    speakCommentFlg = 0;
+    speakMsg.text = 'コメント読み上げをオフにしました。';
+    window.speechSynthesis.speak(speakMsg);
+    return;
+  }
+  speakCommentFlg = 1;
+  speakMsg.text = 'コメント読み上げをオンにしました。';
+  window.speechSynthesis.speak(speakMsg);
+}
+
 var utterance = new SpeechSynthesisUtterance();
 utterance.name = "Google 日本語"
 utterance.lang = "ja-JP"
